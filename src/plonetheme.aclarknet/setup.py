9b96a21fe8a28adc4518b12aece0cbe96621a999
Alex Clark <aclark@aclark.net>
2011-05-23 14:05:52 -0400

    Fix syntax

--- src/plonetheme.aclarknet/setup.py
+++ src/plonetheme.aclarknet/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='plonetheme.aclarknet',
-    version='0.1.0'
+    version='0.1.0',
     install_requires=[
         'setuptools',
         'plone.app.theming',
