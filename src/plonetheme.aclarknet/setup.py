a44c82937f09acb8cb54b1896aac0f9aef242f49
Alex Clark <aclark@aclark.net>
2011-05-23 14:03:58 -0400

    Move jbot templates to package

--- src/plonetheme.aclarknet/setup.py
+++ src/plonetheme.aclarknet/setup.py
@@ -3,4 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name='plonetheme.aclarknet',
     version='0.1.0'
+    install_requires=[
+        'setuptools',
+        'plone.app.theming',
+        'z3c.jbot',
+    ],
 )
