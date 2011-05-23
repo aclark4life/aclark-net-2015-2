8a18a44a263e600696505f0af6ca31a6f14398ce
Alex Clark <aclark@aclark.net>
2011-05-23 16:34:09 -0400

    Diazofy

--- src/plonetheme.aclarknet/setup.py
+++ src/plonetheme.aclarknet/setup.py
@@ -3,6 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name='plonetheme.aclarknet',
     version='0.1.0',
+    packages=find_packages(),
+    namespace_packages=[
+        'plonetheme',
+    ],
+    install_package_data=True,
     install_requires=[
         'setuptools',
         'plone.app.theming',
